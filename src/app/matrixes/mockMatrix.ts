d812fca2a3a20acf0c4ca70e34312c1b2c12d983
iuliavoit <iuliavoit13@yahoo.com>
2023-05-18 18:17:11 +0300

    matrix creation & transpose calc

--- src/app/matrixes/mockMatrix.ts
+++ src/app/matrixes/mockMatrix.ts
@@ -43,35 +43,35 @@ export var mockMatrix =
     }],
     "entity1PaddingData": {
       "scriptbee": {
-        "entity1namecolumn": {"value": "scriptbee", "style": ""},
+        "entity1namecolumn": {"value": "scriptbee", "style": "font-weight:bold;"},
         "entity1sizecolumn": {"value": "695", "style": "background-color:green;"},
         "entity1devscolumn": {"value": "14", "style": ""},
         "entity1activedevscolumn": {"value": "14", "style": ""},
         "entity1apkscolumn": {"value": "14", "style": ""}
       },
       "dx": {
-        "entity1namecolumn": {"value": "dx", "style": ""},
+        "entity1namecolumn": {"value": "dx", "style": "font-weight:bold;"},
         "entity1sizecolumn": {"value": "577", "style": "background-color:green;"},
         "entity1devscolumn": {"value": "50", "style": ""},
         "entity1activedevscolumn": {"value": "50", "style": ""},
         "entity1apkscolumn": {"value": "50", "style": ""}
       },
       "depinder": {
-        "entity1namecolumn": {"value": "depinder", "style": ""},
+        "entity1namecolumn": {"value": "depinder", "style": "font-weight:bold;"},
         "entity1sizecolumn": {"value": "55", "style": ""},
         "entity1devscolumn": {"value": "11", "style": ""},
         "entity1activedevscolumn": {"value": "11", "style": ""},
         "entity1apkscolumn": {"value": "11", "style": ""}
       },
       "depminer": {
-        "entity1namecolumn": {"value": "depminer", "style": ""},
+        "entity1namecolumn": {"value": "depminer", "style": "font-weight:bold;"},
         "entity1sizecolumn": {"value": "19", "style": "color:red;"},
         "entity1devscolumn": {"value": "3", "style": ""},
         "entity1activedevscolumn": {"value": "3", "style": ""},
         "entity1apkscolumn": {"value": "3", "style": ""}
       },
       "insider": {
-        "entity1namecolumn": {"value": "insider", "style": ""},
+        "entity1namecolumn": {"value": "insider", "style": "font-weight:bold;"},
         "entity1sizecolumn": {"value": "201", "style": "background-color:green;"},
         "entity1devscolumn": {"value": "18", "style": ""},
         "entity1activedevscolumn": {"value": "18", "style": ""},
@@ -80,27 +80,27 @@ export var mockMatrix =
     },
     "entity2PaddingData": {
       "scriptbee": {
-        "entity2namecolumn": {"value": "scriptbee", "style": ""},
+        "entity2namecolumn": {"value": "scriptbee", "style": "font-weight:bold;"},
         "entity2knowledgecolumn": {"value": "14", "style": ""},
         "entity2componentscolumn": {"value": "14", "style": ""}
       },
       "dx": {
-        "entity2namecolumn": {"value": "dx", "style": ""},
+        "entity2namecolumn": {"value": "dx", "style": "font-weight:bold;"},
         "entity2knowledgecolumn": {"value": "50", "style": ""},
         "entity2componentscolumn": {"value": "50", "style": ""}
       },
       "depinder": {
-        "entity2namecolumn": {"value": "depinder", "style": ""},
+        "entity2namecolumn": {"value": "depinder", "style": "font-weight:bold;"},
         "entity2knowledgecolumn": {"value": "11", "style": ""},
         "entity2componentscolumn": {"value": "11", "style": ""}
       },
       "depminer": {
-        "entity2namecolumn": {"value": "depminer", "style": ""},
+        "entity2namecolumn": {"value": "depminer", "style": "font-weight:bold;"},
         "entity2knowledgecolumn": {"value": "3", "style": ""},
         "entity2componentscolumn": {"value": "3", "style": ""}
       },
       "insider": {
-        "entity2namecolumn": {"value": "insider", "style": ""},
+        "entity2namecolumn": {"value": "insider", "style": "font-weight:bold;"},
         "entity2knowledgecolumn": {"value": "18", "style": ""},
         "entity2componentscolumn": {"value": "18", "style": ""}
       }
@@ -320,8 +320,3 @@ export var mockMatrix =
       }, {"source": "insider", "target": "insider", "value": "324", "style": "background-color:green;"}]
     }
   }
-
-
-
-
-
